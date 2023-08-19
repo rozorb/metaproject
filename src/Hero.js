e3c1646271ebf60b5276cce270d0094f7cb1d289
rozorb <ronnierozo@yahoo.com>
2023-08-19 16:17:08 -0400

    hero styled

--- src/Hero.js
+++ src/Hero.js
@@ -1,9 +1,20 @@
 import React from "react"
+import dish from "./assets/restauranfood.jpg"
 
 export default function Hero() {
     return(
         <div class="hero">
-          hero
+          <div class="hero-container">
+            <section>
+              <h2 id="hero-heading">Little Lemon</h2>
+              <h4 id="hero-subheading">Chicago</h4>
+              <p id="hero-paragraph">We are a family owned
+               mediterranean restaurant, focused on traditional
+                recipes served with a modern twist. </p>
+              <button id="reserve" >Reserve a table</button>
+            </section>
+            <img id="hero-img" src={dish} alt="food" />
+          </div>
         </div>
     )
 }
