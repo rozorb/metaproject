80669baffb6e299a824a8f4eacf80d8e9fd7b0e2
rozorb <ronnierozo@yahoo.com>
2023-08-25 12:16:10 -0400

    styled testimonial section

--- src/About.js
+++ src/About.js
@@ -3,7 +3,17 @@ import React from "react"
 export default function About() {
     return(
         <div class="about">
-          about
+          <div class="about-container">
+            <section>
+              <h2 id="about-heading">Little Lemon</h2>
+              <h4 id="about-subheading">Chicago</h4>
+              <p id="about-paragraph">We are a family owned
+               mediterranean restaurant, focused on traditional
+                recipes served with a modern twist. 
+              </p>
+            </section>
+            <img  alt="food" />
+          </div>
         </div>
     )
 }
--- src/Testimonials.js
+++ src/Testimonials.js
@@ -1,9 +1,51 @@
 import React from "react"
+import person1 from "./assets/carla.png"
+import person2 from "./assets/steve.png"
+import person3 from "./assets/kaya.png"
+import person4 from "./assets/Benjy.png"
 
 export default function Testimonials() {
     return(
         <div class="test">
-          testimonials
+          <h2 id="test-header">Testimonials</h2>
+          <ul class="reviews">
+            <li>
+              <h4 class="rating">4.8/5</h4>
+              <div class="img-name-container">
+                <img src={person1} class="rev-img" alt="person-1" />
+                <h4>Carla</h4>
+              </div>
+              <p>"The food was fresh, with many flavors. 
+                I was also impressed by the service, its well worth the visit"</p>
+            </li>
+            <li>
+              <h4 class="rating">5/5</h4>
+              <div class="img-name-container">
+                <img src={person2} class="rev-img" alt="person-2" />
+                <h4>Steve</h4>
+              </div>
+              <p>"My new favorite restaurant, my family had a great time.
+                 Definitely try their dessert."</p>
+            </li>
+            <li>
+              <h4 class="rating">4.4/5</h4>
+              <div class="img-name-container">
+                <img src={person3} class="rev-img" alt="person-3" />
+                <h4>Kaya</h4>
+              </div>
+              <p>"Service was excellent, great variety of drinks.
+                 And plenty of choices, even for picky eaters." </p>
+            </li>
+            <li>
+              <h4 class="rating">4.6/5</h4>
+              <div class="img-name-container">
+                <img src={person4} class="rev-img" alt="person-4" />
+                <h4>Ben</h4>
+              </div>
+              <p>"Wine perfect suited the food, the place was cozy.
+                 Definitely have to visit again, for other menu items."</p>
+            </li>
+          </ul>
         </div>
     )
 }
